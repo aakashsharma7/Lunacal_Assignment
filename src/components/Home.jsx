efa3f2b5837365b75b535df287d2ea67010f5333
aakashsharma7 <aakashsharma.ghd@gmail.com>
2024-09-10 12:28:47 +0530

    completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import AboutCard from "./About";
+import Gallery from "./GalleryWidget";
+
+
+export default function Home() {
+    return (
+        <div className="w-full flex flex-row gap-8 justify-between overflow-hidden ">
+            <div className=" font-semibold w-[836px] bg-[#616161D1] border border-[#96BEE7] rounded-[27px] h-[730px]"></div>
+            <div className="w-1/2 flex flex-col gap-10">
+                <AboutCard />
+                <div className="px-14">
+                    <div className="bg-custom-gradient rounded-sm w-full px-6 h-[4px] shadow-custom"></div>
+                </div>
+                <Gallery />
+                <div className="px-14">
+                    <div className="bg-custom-gradient rounded-sm w-full px-6 h-[4px] shadow-custom"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
